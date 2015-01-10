9e0d9535fbd27f8fe8ec1c95f42e635a7e5fc50a
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-01-09 22:18:55 -0800

    Restructuring message parser for more generic handling in core.  Changes to be committed: 	modified:   parser.pl

--- parser.pl
+++ parser.pl
@@ -11,6 +11,98 @@
 	   ,ping_from/2
 	  ]).
 
+
+/*
+
+Documentation Source: http://www.networksorcery.com/enp/protocol/irc.htm
+Message syntax:
+  
+message =	[ ":" prefix SPACE ] command [ params ] crlf	
+prefix =	servername / ( nickname [ [ "!" user ] "@" host ])	
+command =	1*letter / 3digit	
+params =	*14( SPACE middle ) [ SPACE ":" trailing ]	
+=/	14( SPACE middle ) [ SPACE [ ":" ] trailing ]
+
+// Any byte except NUL, CR, LF, " " and ":".
+nospcrlfcl =	%x01-09 / %x0B-0C / %x0E-1F / %x21-39 / %x3B-FF
+middle =	nospcrlfcl *( ":" / nospcrlfcl )	
+trailing =	*( ":" / " " / nospcrlfcl )	
+SPACE =	%x20	; Whitespace.
+crlf =	%x0D %x0A	; Carriage return/linefeed.
+  
+*/
+
+
+%% is_prefix(+S0, -S) is semidet.
+%
+% This will succeed iff a message has a colon as its first character. The rest
+% of the message will be returned as the difference.
+
+is_prefix --> `:`.
+
+%% trailing(+S0, -S) is semidet.
+%
+% This will succeed iff a parameter has a colon as its first character. The
+% rest of the parameter will be returned as the difference.
+
+trailing --> `:`.
+
+
+%--------------------------------------------------------------------------------%
+
+
+%% parse_line(+Line, -Msg) is det.
+%
+% After splitting the line from any potential trailing parameters, format
+% the line in a message compound term for proper reading outside of this
+% module.
+
+parse_line(Line, Msg) :-
+  split_from_trailer(Line, Out),
+  once(fmt_line(Out, Msg)).
+
+
+%% fmt_line(+Line, -Msg) is det.
+%
+% Split a server message into (potentially) 3 parts. The message can be split
+% into a prefix, command, and some command parameters. However, the prefix
+% is optional, and the parameter list can be potentially empty.
+%
+% Msg output is of the format:
+% msg(prefix, command, [parameters...], trailing_parameter).
+
+fmt_line(["", Main, Trailer], msg(Prefix, Cmd, Params, Trailer)) :-
+  split_string(Main, " ", "", [Prefix,Cmd|Params]).
+
+fmt_line(["", Main], msg(Prefix, Cmd, Params)) :-
+  split_string(Main, " ", "", [Prefix,Cmd|Params]).
+
+fmt_line([Main, Trailer], msg(Cmd, Params, Trailer)) :-
+  split_string(Main, " ", "", [Cmd|Params]).
+
+fmt_line([Main], msg(Cmd, Params)) :-
+  split_string(Main, " ", "", [Cmd|Params]).
+
+
+%% split_from_trailer(+Line, -Out) is det.
+%
+% Split the main portion of the message from the trailer portion of the message
+% if a trailer does exist. These are the possibilities when operating
+% under the current IRC protocol:
+%
+% 1) ["", Main, Trailer]
+% 2) ["", Main]
+% 3) [Main, Trailer]
+% 4) [Main]
+
+split_from_trailer(Line, Out) :-
+  split_string(Line, ":", " ", Out).
+
+
+
+
+
+/*
 %--------------------------------------------------------------------------------%
 % irc/ping requests
 %--------------------------------------------------------------------------------%
@@ -119,3 +211,4 @@ title([]) -->
 title([C|T]) -->
   [C], title(T).
 
+*/
