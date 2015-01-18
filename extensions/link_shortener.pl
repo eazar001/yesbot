50b0442752758a9f5c1848061cb81b545342e7e3
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-01-17 17:54:28 -0800

    Moving some "not so generic" utilities back, but still keeping the utils module.  Changes to be committed: 	modified:   extensions/link_shortener.pl 	modified:   utilities.pl

--- extensions/link_shortener.pl
+++ extensions/link_shortener.pl
@@ -15,9 +15,55 @@
 
 :- use_module(dispatch).
 :- use_module(parser).
-:- use_module(utilities).
 :- use_module(library(http/http_open)).
 :- use_module(library(http/http_ssl_plugin)).
+
+
+%--------------------------------------------------------------------------------%
+% Main Extension
+%--------------------------------------------------------------------------------%
+
+
+%% link_shortener(+Msg) is semidet.
+%
+% This link shortener extension will first parse a message to determine whether
+% or not this message contains a link. If a link exists, it will determine to
+% whether it is http or https. It will accordingly attempt to open the URL and
+% extract a title from the from the link. If the link is 100 or more characters
+% in length, then the link will be sent to tinyurl for shortening. The shortened
+% URL will be sent to the channel publicly for general consumption.
+%
+% This predicate will only succeed if Recip is identical to Chan
+
+link_shortener(Msg) :-
+  Msg = msg(_Prefix, "PRIVMSG", [Chan], M),
+  core:connection(_Nick, _Pass, Ch, _Hostname, _Servername, _Realname),
+  atom_string(Ch, Chan),
+  has_link(_, L, M, _),
+  atom_codes(Link, L),
+  length(L, N),
+  (
+     % shorten link if 100 or more characters in length
+     N >= 100 ->
+       make_tiny(Link, Title, Tiny),
+       (
+          Title = [] ->
+            true
+          ;
+            send_msg(priv_msg, Title, Chan)
+       ),
+       send_msg(priv_msg, Tiny, Chan)
+     ;
+       url_get_line(Link, Title) ->
+       (
+          Title = [] ->
+	    true
+          ;
+	    send_msg(priv_msg, Title, Chan)
+       )
+  ),
+  core:get_irc_stream(Stream),
+  flush_output(Stream).
 
 
 %--------------------------------------------------------------------------------%
@@ -91,49 +137,68 @@ visit_url(Link, Reply) :-
 
 
 %--------------------------------------------------------------------------------%
-% Extension
+% Utilities for IRC/Private Mesages
 %--------------------------------------------------------------------------------%
 
 
-%% link_shortener(+Msg) is semidet.
+%% NOTE : It is more robust to actually parse these tags from the whole stream
+%% using library(SGML). However, the disadvantage there is that you lose the
+%% ability to parse lazily and close the connection early. So in a module like
+%% this it is probably more beneficial to performance to implement in a way
+%% that resembles what is below.
+
+%% parse for existence of a link in a sequence of characters
+
+has_link(http, [104,116,116,112,58,47,47|L]) -->
+  `http://`, get_link(http, L), !.
+
+has_link(https, [104,116,116,112,115,58,47,47|L]) -->
+  `https://`, get_link(https, L), !.
+  
+has_link(Protocol, L) -->
+  [_], has_link(Protocol, L).
+
+get_link(_, []) --> [32|_], !.
+
+get_link(Protocol, [C|L]) -->
+  [C], get_link(Protocol, L).
+
+get_link(_, []) --> [].
+
+
+%% html
+%% opening and closing title tag variations
+
+title_open(`<title>`).
+title_open(`<TITLE>`).
+title_open(`<Title>`).
+
+title_close(`</title>`).
+title_close(`</TITLE>`).
+title_close(`</Title>`).
+
+
+%% get_title(-D, +S0, -S) is semidet.
 %
-% This link shortener extension will first parse a message to determine whether
-% or not this message contains a link. If a link exists, it will determine to
-% whether it is http or https. It will accordingly attempt to open the URL and
-% extract a title from the from the link. If the link is 100 or more characters
-% in length, then the link will be sent to tinyurl for shortening. The shortened
-% URL will be sent to the channel publicly for general consumption.
+% Any sequence that contains the opening 'title' tag is consumed. The following
+% characters are parsed as the title until the closing tag is reached. The
+% characters are stored as a list as they are parsed.
+
+get_title(T) -->
+  {title_open(Tag)},
+  Tag, title(T), !.
+
+get_title(T) -->
+  [_], get_title(T).
+
+%% title(-D, +S0, -S) is semidet.
 %
-% This predicate will only succeed if Recip is identical to Chan
+% Consume any sequence containing the closing title tag and succeed by closing
+% the list of stored contents that reside in the tag.
 
-link_shortener(Msg) :-
-  Msg = msg(_Prefix, "PRIVMSG", [Chan], M),
-  core:connection(_Nick, _Pass, Ch, _Hostname, _Servername, _Realname),
-  atom_string(Ch, Chan),
-  has_link(_, L, M, _),
-  atom_codes(Link, L),
-  length(L, N),
-  (
-     % shorten link if 100 or more characters in length
-     N >= 100 ->
-       make_tiny(Link, Title, Tiny),
-       (
-          Title = [] ->
-            true
-          ;
-            send_msg(priv_msg, Title, Chan)
-       ),
-       send_msg(priv_msg, Tiny, Chan)
-     ;
-       url_get_line(Link, Title) ->
-       (
-          Title = [] ->
-	    true
-          ;
-	    send_msg(priv_msg, Title, Chan)
-       )
-  ),
-  core:get_irc_stream(Stream),
-  flush_output(Stream).
+title([]) -->
+  {title_close(Tag)},
+  Tag, !.
 
-
+title([C|T]) -->
+  [C], title(T).
