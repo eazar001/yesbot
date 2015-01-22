172b691a8c13a9de8e17fb0bc76edb1d33f4ba9d
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-01-22 02:10:50 -0800

    Exporting all utilities in link_shortener to a new html submodule.  Changes to be committed: 	modified:   extensions/link_shortener.pl 	new file:   extensions/submodules/html.pl

--- extensions/link_shortener.pl
+++ extensions/link_shortener.pl
@@ -16,6 +16,7 @@
 :- use_module(dispatch).
 :- use_module(parser).
 :- use_module(utilities).
+:- use_module(submodules/html).
 :- use_module(library(http/http_open)).
 :- use_module(library(http/http_ssl_plugin)).
 
@@ -138,69 +139,3 @@ visit_url(Link, Reply) :-
   ).
 
 
-%--------------------------------------------------------------------------------%
-% Utilities for IRC/Private Mesages
-%--------------------------------------------------------------------------------%
-
-
-%% NOTE : It is more robust to actually parse these tags from the whole stream
-%% using library(SGML). However, the disadvantage there is that you lose the
-%% ability to parse lazily and close the connection early. So in a module like
-%% this it is probably more beneficial to performance to implement in a way
-%% that resembles what is below.
-
-%% parse for existence of a link in a sequence of characters
-
-has_link(http, [104,116,116,112,58,47,47|L]) -->
-  `http://`, get_link(http, L), !.
-
-has_link(https, [104,116,116,112,115,58,47,47|L]) -->
-  `https://`, get_link(https, L), !.
-  
-has_link(Protocol, L) -->
-  [_], has_link(Protocol, L).
-
-get_link(_, []) --> [32|_], !.
-
-get_link(Protocol, [C|L]) -->
-  [C], get_link(Protocol, L).
-
-get_link(_, []) --> [].
-
-
-%% html
-%% opening and closing title tag variations
-
-title_open(`<title>`).
-title_open(`<TITLE>`).
-title_open(`<Title>`).
-
-title_close(`</title>`).
-title_close(`</TITLE>`).
-title_close(`</Title>`).
-
-
-%% get_title(-D, +S0, -S) is semidet.
-%
-% Any sequence that contains the opening 'title' tag is consumed. The following
-% characters are parsed as the title until the closing tag is reached. The
-% characters are stored as a list as they are parsed.
-
-get_title(T) -->
-  {title_open(Tag)},
-  Tag, title(T), !.
-
-get_title(T) -->
-  [_], get_title(T).
-
-%% title(-D, +S0, -S) is semidet.
-%
-% Consume any sequence containing the closing title tag and succeed by closing
-% the list of stored contents that reside in the tag.
-
-title([]) -->
-  {title_close(Tag)},
-  Tag, !.
-
-title([C|T]) -->
-  [C], title(T).
