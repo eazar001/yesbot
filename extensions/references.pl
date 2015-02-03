69be2fdf10dc6654aa996011fc74109b21df01a3
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-02-02 21:47:21 -0800

    Refactoring references extension.  Changes to be committed: 	modified:   extensions/references.pl

--- extensions/references.pl
+++ extensions/references.pl
@@ -20,29 +20,11 @@ chan("##prolog").
 references(Msg) :-
   chan(Chan),
   Msg = msg(_Prefix, "PRIVMSG", [Chan], R0),
-  string_codes(R0, R1),
-  is_ref(R1, Value),
-  R1 = [_|R],
+  string_concat("?", R, R0),
+  ref_value(R, Value),
   ref_title(R, Title),
   send_msg(priv_msg, Title, Chan),
   send_msg(priv_msg, Value, Chan).
-  
-
-%--------------------------------------------------------------------------------%
-
-
-%% is_ref(+S0, -S) is semidet.
-%
-% A list of codes is a reference is it starts with an initial sequence and ends
-% with a valid command.
-
-is_ref -->
-  initial,
-  valid_cmd.
-
-
-initial --> `?`.
-valid_cmd --> ref.
 
 
 %--------------------------------------------------------------------------------%
@@ -50,18 +32,18 @@ valid_cmd --> ref.
 %--------------------------------------------------------------------------------%
 
 
-ref(`faq`, "http://www.pathwayslms.com/swipltuts/student/index.html").
-ref(`modules`, "http://michael.richter.name/tutorials/swiplmod").
-ref(`tutorials`, "http://www.pathwayslms.com/swipltuts/").
-ref(`dcg`, "http://www.pathwayslms.com/swipltuts/dcg/index.html").
-ref(`webapps`, "http://www.pathwayslms.com/swipltuts/html/index.html").
-ref(`clpfd`, "http://www.pathwayslms.com/swipltuts/clpfd/clpfd.html").
+ref_value("faq", "http://www.pathwayslms.com/swipltuts/student/index.html").
+ref_value("modules", "http://michael.richter.name/tutorials/swiplmod").
+ref_value("tutorials", "http://www.pathwayslms.com/swipltuts/").
+ref_value("dcg", "http://www.pathwayslms.com/swipltuts/dcg/index.html").
+ref_value("webapps", "http://www.pathwayslms.com/swipltuts/html/index.html").
+ref_value("clpfd", "http://www.pathwayslms.com/swipltuts/clpfd/clpfd.html").
 
-ref_title(`faq`, "Frequently Asked Questions for ##Prolog").
-ref_title(`modules`, "Using SWI-Prolog's Modules").
-ref_title(`tutorials`, "Real World Programming in SWI-Prolog").
-ref_title(`dcg`, "Tutorial -- Using Definite Clause Grammars in SWI-Prolog").
-ref_title(`webapps`, "Tutorial -- Web Applications in SWI-Prolog").
-ref_title(`clpfd`, "http://www.pathwayslms.com/swipltuts/clpfd/clpfd.html").
+ref_title("faq", "Frequently Asked Questions for ##Prolog").
+ref_title("modules", "Using SWI-Prolog's Modules").
+ref_title("tutorials", "Real World Programming in SWI-Prolog").
+ref_title("dcg", "Tutorial -- Using Definite Clause Grammars in SWI-Prolog").
+ref_title("webapps", "Tutorial -- Web Applications in SWI-Prolog").
+ref_title("clpfd", "http://www.pathwayslms.com/swipltuts/clpfd/clpfd.html").
 
 
