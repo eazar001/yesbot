69d04eee14005154fcb55f5befacff293e918321
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-02-21 12:30:16 -0800

    Adding dictionary extension. 	new file:   extensions/dict.pl 	modified:   extensions/wiki_search.pl

--- extensions/dict.pl
+++ extensions/dict.pl
@@ -1,32 +1,30 @@
 
-:- module(wiki_search, [wiki_search/1]).
+:- module(dict, [dict/1]).
 
-:- use_module(library(sgml)).
+:- use_module(dispatch).
 :- use_module(library(http/http_open)).
 :- use_module(library(xpath)).
 :- use_module(library(uri)).
-:- use_module(dispatch).
 
 
 chan("##prolog").
-wiki_start(`http://www.wikipedia.org/search-redirect.php?family=wikipedia&search=`).
-wiki_end(`&language=en&go=Go`).
+dict_start(`http://dictionary.reference.com/browse/`).
+dict_end(`?s=t`).
+
+dict(Msg) :-
+  thread_create(ignore(dict_(Msg)), _Id, [detached(true)]).
 
 
-wiki_search(Msg) :-
-  thread_create(ignore(wiki_search_(Msg)), _Id, [detached(true)]).
-
-
-wiki_search_(Msg) :-
+dict_(Msg) :-
   chan(Chan),
   Msg = msg(_Prefix, "PRIVMSG", [Chan], Text),
-  append(`?wiki `, Q0, Text),
+  append(`?dict `, Q0, Text),
   atom_codes(Atom, Q0),
   uri_encoded(query_value, Atom, Encoded),
   atom_codes(Encoded, Q),
   append(Q, Diff, Query),
-  wiki_start(Start),
-  wiki_end(End),
+  dict_start(Start),
+  dict_end(End),
   append(Start, Query, L),
   Diff = End,
   string_codes(Link, L),
@@ -34,9 +32,9 @@ wiki_search_(Msg) :-
     http_open(Link, Stream, [timeout(20)]),
     (
        load_html(Stream, Content, []),
-       xpath_chk(Content, //p(normalize_space), P0),
+       xpath_chk(Content, //div(@class='def-content', normalize_space), P0),
        atom_codes(P0, P),
-       maplist(wiki_search:change, P, Paragraph),
+       maplist(dict:change, P, Paragraph),
        send_msg(priv_msg, Link, Chan),
        send_msg(priv_msg, Paragraph, Chan)
     ),
@@ -48,5 +46,4 @@ change(10, 32).
 change(X, 32) :- X < 10.
 change(X, 63) :- X > 255.
 change(X, X) :- X > 10, X =< 255.
-
 
