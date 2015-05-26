f431a8e8919a7df78ffe09fa87307abb4b2ebfb5
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-05-26 00:31:07 -0700

    Update user registration info in config.  Changes to be committed: 	modified:   config.pl

--- config.pl
+++ config.pl
@@ -34,6 +34,49 @@
 % DO NOT check in your
 % settings.db file
 
+
+/*
+  For those of you whom are curious about the IRC user registration process:
+  These are relevant excerpts from Section 4.1.3 of the IRC RFC.
+  (This document is available at: https://tools.ietf.org/html/rfc1459)
+  
+  Command: USER
+  Parameters: <username> <hostname> <servername> <realname>
+
+  The USER message is used at the beginning of connection to specify
+  the username, hostname, servername and realname of a new user.  It is
+  also used in communication between servers to indicate new user
+  arriving on IRC, since only after both USER and NICK have been
+  received from a client does a user become registered.
+
+  Between servers USER must to be prefixed with client's NICKname.
+  Note that hostname and servername are normally ignored by the IRC
+  server when the USER command comes from a directly connected client
+  (for security reasons), but they are used in server to server
+  communication.  This means that a NICK must always be sent to a
+  remote server when a new user is being introduced to the rest of the
+  network before the accompanying USER is sent.
+
+  It must be noted that realname parameter must be the last parameter,
+  because it may contain space characters and must be prefixed with a
+  colon (':') to make sure this is recognised as such.
+
+  Since it is easy for a client to lie about its username by relying
+  solely on the USER message, the use of an "Identity Server" is
+  recommended.  If the host which a user connects from has such a
+  server enabled the username is set to that as in the reply from the
+  "Identity Server".
+
+  Numeric Replies:
+
+  ERR_NEEDMOREPARAMS              ERR_ALREADYREGISTRED
+
+  Examples:
+  
+  USER guest tolmoon tolsun :Ronnie Reagan
+*/
+
+
 % Constants for general server connection specs
 :- setting(host, atom, 'chat.freenode.net', 'IRC host to connect to').
 :- setting(port, between(1,0x7FFF), 6667, 'Port to connect to').
@@ -47,11 +90,10 @@
 
 % Constants for user registration specs
 :- setting(bot_hostname, atom, hostname,
-     'User Registration host- name of local server.').
+     'The user''s hostname (usually ignored).').
 
 :- setting(bot_servername, atom, servername,
-     'User Registration server- usually same as host. Use use_host to make it \c
-      same').
+     'The user''s servername (usually ignored).').
 
 :- setting(realname, atom, anonymous, 'Bot owner''s real name').
 
