daabab35b5fc3e8c0b97eebe3ff2399eb8208180
Anniepoo <annie66us@yahoo.com>
2015-08-24 20:10:01 -0700

    Ringer extension.

---
+++ extensions/ringer.pl
@@ -0,0 +1,29 @@
+:- module(ringer, [ringer/1]).
+
+:- use_module(dispatch).
+:- use_module(submodules/emoticon_lib).
+:- use_module(submodules/utils).
+:- use_module(library(settings)).
+:- use_module(library(dcg/basics)).
+
+% target any channel, any bot
+%
+target(_, _).
+
+:- setting(ringer:handle, atom, none, 'IRC nick to alert on').
+:- setting(ringer:shell, atom, 'rem', 'script that will be shelled').
+
+ringer(Msg) :-
+  Msg = msg(_Prefix, "PRIVMSG", _, [63,114,105,110,103,32|Rest]),
+  determine_recipient(ringer, Msg, Rec),
+  setting(ringer:handle, Handle),
+  atom_codes(Handle, CHandle),
+  phrase(contains(CHandle), Rest),
+  setting(ringer:shell, Shell),
+  once(shell(Shell)),
+  send_msg(priv_msg, `party has been rung`, Rec).
+
+contains(Str) -->
+	string(_),
+	Str,
+	string(_).
