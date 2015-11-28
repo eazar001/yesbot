bd7b697c4c98c8b8d210614fdde49f0714e73e9d
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-11-28 15:56:39 -0800

    Adding shutdown and reboot commands to bot_control.

--- extensions/bot_control.pl
+++ extensions/bot_control.pl
@@ -20,17 +20,35 @@ bot_control_(Id-Msg) :-
 
 
 control(Me-Chan, '?stop ', Arg) :-
-  Arg \== bot_control,
-  valid_extensions([Arg]),
+  (  Arg \== bot_control,
+     extensions(Current, _),
+     memberchk(Arg, Current),
+     valid_extensions([Arg])
+  -> true
+  ;  priv_msg(Me, "I'm afraid I can't do that, Dave", Chan),
+     !, fail
+  ),
   remove_extensions([Arg]),
   format(string(Report), "Okay, stopping ~s", [Arg]),
   priv_msg(Me, Report, Chan).
 
 control(Me-Chan, '?start ', Arg) :-
-  Arg \== bot_control,
-  valid_extensions([Arg]),
-  extensions(Current, _),
-  \+memberchk(Arg, Current),
+  (  Arg \== bot_control,
+     valid_extensions([Arg]),
+     extensions(Current, _),
+     \+memberchk(Arg, Current)
+  -> true
+  ;  priv_msg(Me, "I'm afraid I can't do that, Dave", Chan),
+     !, fail
+  ),
   add_new_extensions([Arg]),
   format(string(Report), "Okay, starting ~s", [Arg]),
   priv_msg(Me, Report, Chan).
+
+control(Me-Chan, '?reboot', '') :-
+  priv_msg(Me, "Okay, rebooting.", Chan),
+  thread_signal(irc, throw(abort)).
+
+control(Me-Chan, '?off', '') :-
+  priv_msg(Me, "Okay, shutting down.", Chan),
+  thread_signal(irc, halt).
