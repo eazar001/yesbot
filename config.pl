381d81b1bf39875c8b02cc6c196d0eface677b4c
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-11-28 13:24:35 -0800

    Adding the output extension to configuration default.
    This is will be a more user-friendly default for any first time users of
    yesbot.

--- config.pl
+++ config.pl
@@ -60,7 +60,7 @@
 :- setting(chans, list(text), [ '##prolog', '##math' ],
      'List of channels to connect to').
 
-:- setting(extensions, list(atom), [link_shortener, sync_chat_log],
+:- setting(extensions, list(atom), [output, link_shortener, sync_chat_log],
      'list of extensions to load').
 
 % Constants for user registration specs
