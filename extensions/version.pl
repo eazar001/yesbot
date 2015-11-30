0491b75804fb70b160ded89b8b991da916fe6f0d
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-11-30 13:44:21 -0800

    Adding in-channel version requests.

---
+++ extensions/version.pl
@@ -0,0 +1,20 @@
+:- module(yesbot_version, [yesbot_version/1]).
+
+:- use_module(library(irc_client)).
+:- use_module(submodules/utils).
+
+:- dynamic yesbot_version/1.
+
+target("##prolog", "yesbot").
+
+yesbot_version(Msg) :-
+  thread_create(ignore(yesbot_version_(Msg)), _, [detached(true)]).
+
+yesbot_version_(Me-Msg) :-
+  Msg = msg(_Prefix, "PRIVMSG", _, Codes),
+  atom_codes(Atom, Codes),
+  normalize_space(string("?version"), Atom),
+  determine_recipient(yesbot_version, Msg, Recipient),
+  yesbot_version(Vsn),
+  format(string(Report), "yesbot version ~a", [Vsn]),
+  priv_msg(Me, Report, Recipient).
