08dea67f7820cda5f7cad0844a43e5ab654f40b5
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2017-03-05 10:51:53 -0800

    Adding support for error messages in prolog_eval.

--- extensions/prolog_eval.pl
+++ extensions/prolog_eval.pl
@@ -10,38 +10,66 @@
 target("##prolog", "yesbot").
 
 prolog_eval(Msg) :-
-  thread_create(ignore(prolog_eval_(Msg)), _, [detached(true)]).
+	thread_create(ignore(prolog_eval_(Msg)), _, [detached(true)]).
 
 
 prolog_eval_(Me-Msg) :-
-  Msg = msg(_Prefix, "PRIVMSG", _, Text),
-  append(`?-`, Rest, Text),
-  string_codes(String, Rest),
-  term_string(Goal, String, [variable_names(Vars)]),
-  safe_goal(Goal),
-  call_with_time_limit(10, findall(Vars, limit(7, Goal), Sols)),
-  determine_recipient(prolog_eval, Msg, Recip),
-  maplist(include(nonvar_binding), Sols, Nonvars),
-  evaluate(Me, Recip, Nonvars).
+	Msg = msg(_Prefix, "PRIVMSG", _, Text),
+	append(`?-`, Rest, Text),
+	string_codes(String, Rest),
+	term_string(Goal, String, [variable_names(Vars)]),
+	catch(
+		safe_goal(Goal),
+		Error,
+		(	message_to_string(Error, ErrorString),
+			determine_recipient(prolog_eval, Msg, Recip),
+			priv_msg(Me, ErrorString, Recip),
+			fail
+		)
+	),
+	catch(
+		call_with_time_limit(10, findall(Vars, limit(7, Goal), Sols)),
+		Error,
+		(	message_to_string(Error, ErrorString),
+			determine_recipient(prolog_eval, Msg, Recip),
+			priv_msg(Me, ErrorString, Recip),
+			fail
+		)
+	),
+	call_with_time_limit(10, findall(Vars, limit(7, Goal), Sols)),
+	determine_recipient(prolog_eval, Msg, Recip),
+	maplist(include(nonvar_binding), Sols, Nonvars),
+	evaluate(Me, Recip, Nonvars).
 
 
 evaluate(Me, Recip, []) :-
-  priv_msg(Me, "no.", Recip).
+	priv_msg(Me, "no.", Recip).
 
 evaluate(Me, Recip, [[]]) :-
-  priv_msg(Me, "yes.", Recip).
+	priv_msg(Me, "yes.", Recip).
 
 evaluate(Me, Recip, Sols) :-
-  maplist(term_string, Sols, Strings),
-  maplist(string_codes, Strings, Codes),
-  maplist(\Code^F^(Code=[_|R], format_sols(R,F)), Codes, FormattedCodes),
-  maplist(\Msg^(priv_msg(Me, Msg, Recip), sleep(1)), FormattedCodes).
+	maplist(term_string, Sols, Strings),
+	maplist(string_codes, Strings, Codes),
+	maplist(\Code^F^(Code=[_|R], format_sols(R,F)), Codes, FormattedCodes),
+	maplist(\Msg^(priv_msg(Me, Msg, Recip), sleep(1)), FormattedCodes).
 
 
 format_sols([_], []).
 format_sols([X|Xs], [X|Ys]) :-
-  format_sols(Xs, Ys).
+	format_sols(Xs, Ys).
 
 
 nonvar_binding(_=Binding) :-
-  nonvar(Binding).
+	nonvar(Binding).
+
+evaluate_with_errors(Me-Msg, Goal) :-
+	catch(
+		Goal,
+		Error,
+		(	message_to_string(Error, ErrorString),
+			determine_recipient(prolog_eval, Msg, Recip),
+			priv_msg(Me, ErrorString, Recip),
+			fail
+		)
+	).
